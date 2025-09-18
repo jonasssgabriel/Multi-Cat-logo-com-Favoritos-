9e965b59833fbcff350ccdb83a095df355cf2354
jonasssgabriel <jonassgabriel433@gmail.com>
2025-09-17 23:32:22 -0300

    Interface atualizada

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav style={{ padding: "1rem", background: "#898989" }}>
+    <nav style={{ padding: "0.2rem", background: "#898989" }}>
       <div className="app-container">
-        <Link to="/animais">Animais</Link>{" | "}
-        <Link to="/livros">Livros</Link>{" | "}
-        <Link to="/pessoas">Pessoas</Link>{" | "}
+        <Link to="/animais">Animais</Link>{"  "}
+        <Link to="/livros">Livros</Link>{"  "}
+        <Link to="/pessoas">Pessoas</Link>{"  "}
         <Link to="/favoritos">Favoritos</Link>
       </div>
     </nav>
